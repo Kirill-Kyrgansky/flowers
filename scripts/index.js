05b5fbaba428801436668fe009763952be374097
kyrgansky.ku <kyrgansky.ku@zdmail.com>
2023-03-15 21:41:55 +0300

    добавлена анимация

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,52 @@
+let carotenoid = document.querySelector('#carotenoid')
+let anthocyanins = document.querySelector('#anthocyanins')
+let chlorophyll = document.querySelector('#chlorophyll')
+let errorCarotenoid = 'error'
+let titleAnthocyanins = 'error'
+let errorChlorophyll = 'error'
+let title = 'title-2'
 
-setInterval(function () {
-    const show = document.querySelector('span[data-show]')
-    const next = show.nextElementSibling || document.querySelector('span:first-child')
-    const up = document.querySelector('span[data-up]')
-
-    if (up) {
-        up.removeAttribute('data-up')
+document.body.addEventListener('click', e => {
+    if (e.target.tagName === 'circle') {
+        if (e.target.id === 'carotenoid-done') {
+            errorCarotenoid = title
+            e.target.className.baseVal = 'circle red circle-good'
+        } else if (e.target.id === 'carotenoid-error') {
+            carotenoid.className = errorCarotenoid
+            setTimeout(() => {
+                carotenoid.classList.remove(errorCarotenoid)
+                carotenoid.className = title
+            }, 300)
+        }
     }
+})
 
-    show.removeAttribute('data-show')
-    show.setAttribute('data-up', '')
+document.body.addEventListener('click', e => {
+    if (e.target.tagName === 'circle') {
+        if (e.target.id === 'anthocyanins-done') {
+            titleAnthocyanins = title
+            e.target.className.baseVal = 'circle blue circle-good'
+        } else if (e.target.id === 'anthocyanins-error') {
+            anthocyanins.className = titleAnthocyanins
+            setTimeout(() => {
+                anthocyanins.classList.remove(titleAnthocyanins)
+                anthocyanins.className = title
+            }, 300)
+        }
+    }
+})
 
-    next.setAttribute('data-show', '')
-}, 1500)
-
-
-
+document.body.addEventListener('click', e => {
+    if (e.target.tagName === 'circle') {
+        if (e.target.id === 'chlorophyll-done') {
+            errorChlorophyll = title
+            e.target.className.baseVal = 'circle green circle-good'
+        } else if (e.target.id === 'chlorophyll-error') {
+            chlorophyll.className = errorChlorophyll
+            setTimeout(() => {
+                chlorophyll.classList.remove(errorChlorophyll)
+                chlorophyll.className = title
+            }, 300)
+        }
+    }
+})
